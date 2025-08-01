39ba47543e9e9500c2866c5935b1cbf50ef09f7a
Johan Delhomme Montorfano <me@johanmontorfano.com>
2025-08-01 21:54:25 +0200

    fix: body background

--- app/layout.tsx
+++ app/layout.tsx
@@ -5,7 +5,7 @@ import { Navbar } from "@/components/navbar";
 export default function RootLayout(props: { children: React.ReactNode }) {
     return (
         <html lang="en">
-            <body className="antialiased">
+            <body className="antialiased flex flex-col min-h-screen bg-base-200">
                 <Navbar />
                 {props.children}
             </body>
