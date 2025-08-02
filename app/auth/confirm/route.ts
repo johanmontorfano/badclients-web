51aa66a4b674e06046d93833b417f87abab490d4
Johan Delhomme Montorfano <me@johanmontorfano.com>
2025-08-02 12:50:33 +0200

    fix: inconsistencies

--- app/auth/confirm/route.ts
+++ app/auth/confirm/route.ts
@@ -1,8 +1,9 @@
 "use server";
 
 import { type EmailOtpType } from "@supabase/supabase-js";
-import { type NextRequest, NextResponse } from "next/server";
+import { NextRequest, NextResponse } from "next/server";
 import { createClient } from "@/utils/supabase/server";
+import { origin } from "@/utils/origin";
 
 // validate email addresses for new users
 export async function GET(request: NextRequest) {
@@ -10,22 +11,12 @@ export async function GET(request: NextRequest) {
     const token_hash = searchParams.get("token_hash");
     const type = searchParams.get("type") as EmailOtpType | null;
 
-    const redirectTo = request.nextUrl.clone();
-    redirectTo.pathname = "/auth/login";
-    redirectTo.searchParams.delete("token_hash");
-    redirectTo.searchParams.delete("type");
-
     if (token_hash && type) {
         const supabase = await createClient();
         const { error } = await supabase.auth.verifyOtp({ type, token_hash });
 
-        if (!error) {
-            redirectTo.searchParams.set("error", "email_ok");
-            redirectTo.searchParams.delete("next");
-            return NextResponse.redirect(redirectTo);
-        }
+        if (!error)
+            return NextResponse.redirect(origin + "/auth/profile?error=email_ok");
     }
-
-    redirectTo.pathname = "/error";
-    return NextResponse.redirect(redirectTo);
+    return NextResponse.redirect(origin + "/error");
 }
