3bce7bf7a9b03aa852e6727212cf5c903d2507a1
Johan Delhomme Montorfano <me@johanmontorfano.com>
2025-08-02 19:47:27 +0200

    add: support for anon to perma acct upgrade with credits recovery

--- app/auth/actions.ts
+++ app/auth/actions.ts
@@ -4,9 +4,25 @@ import { revalidatePath } from "next/cache";
 import { redirect } from "next/navigation";
 import { createClient } from "@/utils/supabase/server";
 import { PlanTiers } from "@/utils/stripe/plans";
+import { UserMetadata } from "@/utils/supabase/users";
+
+// Will return the credits used by the anonymous session and destroy the acct
+async function handleAnonymousSession() {
+    const supabase = await createClient();
+    const user = await supabase.auth.getUser();
+
+    if (user.data.user && user.data.user.is_anonymous) {
+        await supabase.auth.admin.deleteUser(user.data.user.id);
+        return (user.data.user.user_metadata as UserMetadata).usage || 0;
+    }
+    return 0;
+}
 
 export async function login(formData: FormData) {
     const supabase = await createClient();
+
+    await handleAnonymousSession();
+
     const { error } = await supabase.auth.signInWithPassword({
         email: formData.get("email") as string,
         password: formData.get("password") as string,
@@ -22,6 +38,7 @@ export async function login(formData: FormData) {
 
 export async function signup(formData: FormData) {
     const supabase = await createClient();
+    const creditsUsed = await handleAnonymousSession();
     const { error } = await supabase.auth.signUp({
         email: formData.get("email") as string,
         password: formData.get("password") as string,
@@ -30,7 +47,7 @@ export async function signup(formData: FormData) {
                 nickname: formData.get("nickname") as string,
                 planType: PlanTiers.Free,
                 usageLastReset: Date.now(),
-                usage: 0,
+                usage: creditsUsed,
             },
         },
     });
