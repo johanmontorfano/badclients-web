f59891fd0280728d85f4a0dfaf3dd9c4a29f9d11
Johan Delhomme Montorfano <me@johanmontorfano.com>
2025-08-02 15:22:50 +0200

    add: plan usagesé

--- utils/stripe/plans.ts
+++ utils/stripe/plans.ts
@@ -9,3 +9,20 @@ export const planPrices: Record<PlanTiers, string> = {
     [PlanTiers.Hunter]: process.env.STRIPE_PLAN_PRICE_HUNTER!,
     [PlanTiers.Lifetime]: process.env.STRIPE_PLAN_PRICE_LIFETIME!,
 };
+
+export const planUsage: Record<
+    PlanTiers, { usage: number, timeRange: "day" | "month" }
+> = {
+    [PlanTiers.Free]: {
+        usage: 3,
+        timeRange: "day"
+    },
+    [PlanTiers.Hunter]: {
+        usage: 500,
+        timeRange: "month"
+    },
+    [PlanTiers.Lifetime]: {
+        usage: 750,
+        timeRange: "month"
+    }
+}
