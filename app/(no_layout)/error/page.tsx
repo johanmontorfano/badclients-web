da697410586545fd4e2f0be3afba533b0f310373
Johan Delhomme Montorfano <me@johanmontorfano.com>
2025-08-08 18:27:05 +0200

    add: uncaught error UIs

---
+++ app/(no_layout)/error/page.tsx
@@ -0,0 +1,7 @@
+"use client";
+
+import { ErrorBoundaryUI } from "@/components/error/boundary";
+
+export default function Page() {
+    return <ErrorBoundaryUI error={{ name: "", message: "" }} reset={() => {}} />
+}
