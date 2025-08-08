4b5517f7cc3e63f2dffd4d68385feffcbad258b4
Johan Delhomme Montorfano <me@johanmontorfano.com>
2025-08-08 17:05:11 +0200

    fix: x-pathname was not set in headers

--- middleware.ts
+++ middleware.ts
@@ -13,6 +13,8 @@ export async function middleware(req: NextRequest) {
     const headers = new Headers(req.headers);
     let cookies: any[] = [];
 
+    headers.set("x-pathname", req.nextUrl.pathname);
+
     const supabase = createServerClient(
         process.env.NEXT_PUBLIC_SUPABASE_URL!,
         process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
