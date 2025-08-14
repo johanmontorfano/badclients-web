8df4c930bee472c37b3a9914a8169e9c09d8ba10
Johan Delhomme Montorfano <me@johanmontorfano.com>
2025-08-14 14:17:09 +0200

    add: multi-logo button

--- components/home/download_button.tsx
+++ components/home/download_button.tsx
@@ -1,7 +1,7 @@
 import Image from "next/image";
 
 export function DownloadExtensionButton(props: {
-    icon: string;
+    icon: string[] | string;
     name: string;
     to: string;
 }) {
@@ -10,15 +10,28 @@ export function DownloadExtensionButton(props: {
             href={props.to}
             target="_blank"
             rel="noopener noreferrer"
-            className={`btn w-full rounded-sm py-8 ${props.to === "/" ? "btn-disabled" : "" }`}
+            className={`btn w-full rounded-sm py-8 ${props.to === "/" ? "btn-disabled" : ""}`}
         >
-            <Image
-                alt="download-icon"
-                src={props.icon}
-                width={30}
-                height={30}
-                className="mr-2"
-            />
+            {typeof props.icon !== "string" ? (
+                props.icon.map((i) => (
+                    <Image
+                        key={i}
+                        alt="download-icon"
+                        src={i}
+                        width={30}
+                        height={30}
+                        className="mr-2"
+                    />
+                ))
+            ) : (
+                <Image
+                    alt="download-icon"
+                    src={props.icon}
+                    width={30}
+                    height={30}
+                    className="mr-2"
+                />
+            )}
             Add to {props.name}
         </a>
     );
